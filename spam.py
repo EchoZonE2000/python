fb1801e9681e725cbf67463ead7c4b0ee080d7bf
EchoZonE2000 <echozone@t-online.de>
2019-10-27 23:42:30 +0100

    erster versuch

---
+++ spam.py
@@ -0,0 +1,16 @@
+from random import shuffle
+
+liste="amazing gorgeous blazing stunning tremendous greatest best fantastic phenomenal \
+        delightful ambitious outstanding incredible spectacular super cool magical \
+        revolutionary beautiful jaw-dropping".upper().split()
+shuffle(liste)
+
+for strophe in range(5):
+    for n in range(2):
+        for i in range(4):
+            print("SPAM ",end='')
+        print()
+    
+    print(liste.pop() +" SPAM, "+liste.pop() +" SPAM")
+    print()
+
